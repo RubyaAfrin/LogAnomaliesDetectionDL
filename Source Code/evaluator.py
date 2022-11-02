fc124276e931f187db3c16c8122a3cb0044a0013
Tamanna <tamannacsebuet@gmail.com>
2022-11-02 14:49:04 -0600

    Added code for training the model
    I have added a function for training the model and set hyperparameters.
    
    Hyperparameters:
    1. EMBEDDING_DIM 
    2. batch_size =
    3. epochs 
    4. rnn_units 
    
    Model:
    1. Created a Sequential Model 
    2. Added Masking Layer
    3. Added Bi-LSTM Layer
    4. Added Dense layer and used softmax for activation
    
    Loss Function: binary_crossentropy
    Optimizer: rmsprop

--- Source Code/evaluator.py
+++ Source Code/evaluator.py
@@ -4,6 +4,11 @@ import sys
 import pandas as pd
 from collections import defaultdict
 import scipy.misc
+import numpy as np
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Bidirectional, Masking
+from Attention import Attention
+from dataloader import DataGenerator
 
 
 def evaluate(groundtruth, parsedresult):
@@ -88,7 +93,35 @@ def get_accuracy(series_groundtruth, series_parsedlog, debug=False):
 
 
 
+def train_model():
+    # hyper-parameters
+    EMBEDDING_DIM = 768
+    batch_size = 32
+    epochs = 500
+    rnn_units = 256
 
+    # load data
+    training_data = np.load('iput_data')
+    x_train = training_data['x']
+    y_train = training_data['y']
+    del training_data
+
+    # model
+    model = Sequential()
+    model.add(Masking(mask_value=0., input_shape=(None, EMBEDDING_DIM)))
+    model.add(Bidirectional(LSTM(rnn_units, return_sequences=True)))
+    model.add(Attention(bias=False))
+    model.add(Dense(2, activation='softmax'))
+    model.compile(loss='binary_crossentropy',
+                  optimizer='rmsprop', metrics=['acc'])
+    print(model.summary())
+
+    # train
+    train_generator = DataGenerator(x_train, y_train, batch_size=batch_size)
+    model.fit(train_generator, epochs=epochs)
+
+    # save model
+    model.save('my_model.h5')
 
 
 
